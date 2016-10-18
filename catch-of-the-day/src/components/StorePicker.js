2ed06784517df9c46d0698c1f97417dd24917f52
anik3tra0 <anik3tra0@gmail.com>
2016-10-18 12:06:17 +0530

    Completed Episode 11 - Events & Component Class Bindings

--- catch-of-the-day/src/components/StorePicker.js
+++ catch-of-the-day/src/components/StorePicker.js
@@ -2,12 +2,22 @@ import React from 'react';
 import { getFunName } from '../helpers'
 
 class StorePicker extends React.Component {
+
+	goToStore(event) {
+		event.preventDefault();
+		console.log("Noe You See Me!");
+		// Two Things
+		// 	Grab Input Value
+		console.log(this.storeInput.value);
+		// 	Redirect to store with store id
+	}
+
 	render() {
 		// Always comment outside of JSX
 		return (
-			<form className="store-selector">
+			<form className="store-selector" onSubmit={this.goToStore.bind(this)}>
 				<h2>Please Enter A Store</h2>
-				<input type="text" required placeholder="Store Name" defaultValue={getFunName()}/>
+				<input type="text" required placeholder="Store Name" defaultValue={getFunName()} ref={(input) => { this.storeInput = input}} />
 				<button type="submit">Visit Store -></button>
 			</form>
 		)

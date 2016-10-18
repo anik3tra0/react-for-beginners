8b8cb9b5e2a62e9e7ce60044375c626e298e1771
anik3tra0 <anik3tra0@gmail.com>
2016-10-18 22:46:35 +0530

    Completed Episode 18 - Sync With Firebase

---
+++ catch-of-the-day/src/base.js
@@ -0,0 +1,10 @@
+import Rebase from 're-base';
+
+
+const base = Rebase.createClass({
+  apiKey: "AIzaSyAGpeeI80TEALyBJ_bgDx-W70istNz1-x8",
+  authDomain: "catch-of-the-day-afc20.firebaseapp.com",
+  databaseURL: "https://catch-of-the-day-afc20.firebaseio.com"
+});
+
+export default base;

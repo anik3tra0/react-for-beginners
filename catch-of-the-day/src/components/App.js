0839c72c94f87bb62263ef2865f467a11fe57a2b
anik3tra0 <anik3tra0@gmail.com>
2016-10-18 15:22:16 +0530

    Completed Episode 13 - States

--- catch-of-the-day/src/components/App.js
+++ catch-of-the-day/src/components/App.js
@@ -4,6 +4,22 @@ import Order from './Order';
 import Inventory from './Inventory';
 
 class App extends React.Component {
+  constructor() {
+    super();
+    // initial state
+    this.addFish = this.addFish.bind(this);
+    this.state = {
+      fishes: {},
+      order: {}
+    }
+  }
+
+  addFish(fish) {
+    const fishes = {...this.state.fishes};
+    const timestamp = Date.now();
+    fishes[`fish-${timestamp}`] = fish;
+    this.setState({fishes});
+  }
  render() {
    // Always comment outside of JSX
    return (
@@ -12,7 +28,7 @@ class App extends React.Component {
          <Header tagline="Fresh Seafood Market"/>
        </div>
       <Order />
-      <Inventory />
+      <Inventory addFish={this.addFish} />
      </div>
    )
 
